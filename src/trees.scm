8c3eee6e298a86e1630a5c2f8233adabee2d3a7b
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2017-11-16 22:59:08 +0500

    added some trees exs

--- src/trees.scm
+++ src/trees.scm
@@ -1,4 +1,5 @@
 (load "utils/print.scm")
+(load "utils/maths.scm")
 
 (define (count-leaves x)
     (cond
@@ -24,4 +25,31 @@
 ; (define list-test (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 ; (print (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr list-test)))))))))))))
 
-2.26
+; 2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+; (print (append x y))
+; (1 2 3 4 5 6)
+; (print (cons x y))
+; ((1 2 3) 4 5 6)
+; (print (list x y))
+; ((1 2 3) (4 5 6))
+
+; 2.27
+; TODO
+; (define (reverse list-instance)
+;     (let ((list-length (length list-instance)))
+;         (define (iter counter result)
+;             (if (= counter 0)
+;                 result
+;                 (iter (dec counter) (append result (list (list-ref list-instance (dec counter)))))))
+;         (iter list-length (list))))
+
+; (define (deep-reverse seq)
+;     ())
+
+; (print (deep-reverse (list 1 (list 3 6 1) 6)))
+
+; 2.28
+
