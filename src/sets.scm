e4e3a0e7c11abb12873de91a878e7398cc217781
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2017-11-27 22:31:42 +0500

    completed 2.6

--- src/sets.scm
+++ src/sets.scm
@@ -1,8 +1,9 @@
 (load "utils/print.scm")
+(load "trees.scm")
 
 (define (element-of-set? x set)
     (cond
-        ((null? set) false)
+        ((null? set) #f)
         ((equal? x (car set)) #t)
         (else (element-of-set? x (cdr set)))))
 
@@ -19,10 +20,23 @@
                 (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
 
+; 2.59
 (define (union-set set1 set2)
-    (cond
-        ((null? set1) set2)
-        ((null? set2) set1)
-        (else (union-set (cons (car set1) set2) set2))))
+    (accumulate adjoin-set set2 set1))
 
-(print (union-set '(1 2 3) '(a b c)))
+; (print (union-set '(1 2 3) '(a b c)))
+
+; 2.60
+(define sample-set '(2 3 2 1 3 2 2))
+
+(define (element-of-set-multi? x set)
+    (element-of-set? x set))
+
+(define (adjoin-set-multi x set)
+    (cons x set))
+
+(define (union-set-multi set1 set2)
+    (accumulate adjoin-set set2 set1))
+
+(define (intersection-set-multi set1 set2)
+    (intersection-set-multi set1 set2))
