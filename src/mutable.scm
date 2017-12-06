e0c7c7753a0723c4d13efd38eaf6b9b7bdcc6e50
Dmitrii Skurikhin <skurikhin@tochka.com>
2017-12-06 16:45:21 +0500

    added password-alert

--- src/mutable.scm
+++ src/mutable.scm
@@ -57,3 +57,34 @@
 ; (print ((acc 'withdraw 'qwerty) 20))
 ; (print ((acc 'deposit 'qwerty) 30))
 ; (print ((acc 'withdraw 'qq) 30))
+
+; 3.4
+(define (make-protected-account-alert balance initial-password)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Недостаточно денег на счете"))
+    (define (deposit amount)
+        (set! balance (+ balance amount)) balance)
+    (let ((invalid-password-counter 0))
+        (lambda (m password)
+            (cond
+                ((eq? password initial-password)
+                    (begin (set! invalid-password-counter 0)
+                        (cond
+                            ((eq? m 'withdraw) withdraw)
+                            ((eq? m 'deposit) deposit)
+                            (else (error "Неизвестный вызов -- MAKE-ACCOUNT" m)))))
+                ((= invalid-password-counter 4) (error "Password failed, We've called the police!!!"))
+                (else (begin
+                    (set! invalid-password-counter (inc invalid-password-counter))
+                    (lambda (args) (list "Password failed" "Remained attemts: " (- 5 invalid-password-counter)))))))))
+
+(define acc-alert
+    (make-protected-account-alert 200 'qq))
+; (print ((acc-alert 'withdraw 'qq) 20))
+; (print ((acc-alert 'withdraw '1) 20))
+; (print ((acc-alert 'withdraw '11) 20))
+; (print ((acc-alert 'withdraw '1q) 20))
+; (print ((acc-alert 'withdraw '1ee) 20))
+; (print ((acc-alert 'withdraw '1a) 20))
