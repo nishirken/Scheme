5ad6842895bf8e928d835d1bfc80f222e6f0437c
Dmitrii Skurikhin <skurikhin@tochka.com>
2017-12-06 15:58:27 +0500

    added protected-account

--- src/mutable.scm
+++ src/mutable.scm
@@ -1,6 +1,20 @@
 (load "utils/print.scm")
 (load "utils/maths.scm")
 
+(define (make-account balance)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Недостаточно денег на счете"))
+    (define (deposit amount)
+        (set! balance (+ balance amount)) balance)
+    (define (dispatch m)
+        (cond
+            ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            (else (error "Неизвестный вызов -- MAKE-ACCOUNT" m))))
+    dispatch)
+
 ; 3.2
 (define (make-monitored f)
     (let ((counter 0))
@@ -19,3 +33,27 @@
 ; (print (s 'how-many-calls?))
 ; (print (s 'reset-count))
 ; (print (s 'how-many-calls?))
+
+; 3.3
+(define (make-protected-account balance initial-password)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount)) balance)
+            "Недостаточно денег на счете"))
+    (define (deposit amount)
+        (set! balance (+ balance amount)) balance)
+    (define (dispatch m password)
+        (if (eq? password initial-password)
+            (cond
+                ((eq? m 'withdraw) withdraw)
+                ((eq? m 'deposit) deposit)
+                (else (error "Неизвестный вызов -- MAKE-ACCOUNT" m)))
+            (error "Password failed" password)))
+    dispatch)
+
+(define acc
+    (make-protected-account 100 'qwerty))
+
+; (print ((acc 'withdraw 'qwerty) 20))
+; (print ((acc 'deposit 'qwerty) 30))
+; (print ((acc 'withdraw 'qq) 30))
