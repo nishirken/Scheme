31b3287c65f56e2b19ce790ded66027264dcce60
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2017-12-21 09:03:42 +0500

    added or-gate in digital schemes

--- src/digitalSchemes.scm
+++ src/digitalSchemes.scm
@@ -40,3 +40,29 @@
                         (set-signal! output new-value)))))
     (add-action! a1 and-action-procedure)
     (add-action! a2 and-action-procedure) 'ok)
+
+; 3.28
+(define (logical-and x y)
+    (cond
+        (or (and (= x 0) (= y 0))
+            (and (= x 0) (= y 1))
+            (and (= x 1) (= y 0)) 0)
+        ((and (= x 1) (= y 1)) 1)
+        (else (error "Неправильный сигнал") '(x y))))
+
+(define (logical-or x y)
+    (cond
+        (or (and (= x 1) (= y 1))
+            (and (= x 0) (= y 1))
+            (and (= x 1) (= y 0)) 0)
+        ((and (= x 0) (= y 0)) 0)
+        (else (error "Неправильный сигнал") '(x y))))
+
+(define (or-gate a1 a2 output)
+    (define (or-action-procedure)
+        (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+            (after-delay or-gate-delay
+                    (lambda ()
+                        (set-signal! output new-value)))))
+    (add-action! a1 or-action-procedure)
+    (add-action! a2 or-action-procedure) 'ok)
