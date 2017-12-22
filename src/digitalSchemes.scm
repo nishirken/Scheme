283aca6a6364c9efbb5e61c4c19f6f6be9118d52
Dmitrii Skurikhin <skurikhin@tochka.com>
2017-12-22 13:42:16 +0500

    added wire functions

--- src/digitalSchemes.scm
+++ src/digitalSchemes.scm
@@ -1,5 +1,37 @@
 (load "utils/print.scm")
 
+(define (make-wire)
+    (let ((signal-value 0) (action-procedures '()))
+        (define (set-my-signal! new-value)
+            (if (not (= signal-value new-value))
+                (begin (set! signal-value new-value)
+                    (call-each action-procedures))
+                'done))
+        (define (accept-action-procedure! proc)
+            (set! action-procedures (cons proc action-procedures))
+            (proc))
+        (define (dispatch m)
+            (cond
+                ((eq? m 'get-signal) signal-value)
+                ((eq? m 'set-signal!) set-my-signal!)
+                ((eq? m 'add-action!) accept-action-procedure!)
+                (else (error "Неизвестная операция -- WIRE" m))))
+        dispatch))
+
+(define (call-each procedures)
+    (if (null? procedures)
+        'done
+        (begin ((car procedures)) (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+    (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+    ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+    ((wire 'add-action!) action-procedure))
+
 (define (half-adder a b s c)
     (let ((d (make-wire)) (e (make-wire)))
         (or-gate a b d)
